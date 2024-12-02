c0e8efc0115910d6423567c3794f2201f3def615
Dillon Smart <dillonsmart00@gmail.com>
2024-12-02 19:16:53 +0000

    Added the arguments switch

--- tracker.sh
+++ tracker.sh
@@ -1,3 +1,30 @@
 #!/usr/bin/env bash
 
-tasksCsv="./tasks.csv"
+tasks_csv="${PWD}/tasks.csv"
+action=$1
+
+# Create tasks.csv if it doesn't exist
+if [ ! -f "$tasks_csv" ]; then
+	touch "$tasks_csv"
+fi
+
+case $action in
+  add)
+    task=$2
+    # TODO Add additional column for datetime
+    echo "$task" >> "$tasks_csv"
+    echo "Task added: $task"
+    ;;
+  list)
+    cat -n "$tasks_csv"
+    ;;
+  complete)
+    task_number=$2
+    task=$(sed -n "${task_number}p" "$tasks_csv")
+    sed -i "${task_number}d" "$tasks_csv"
+    echo "Task completed: $task"
+    ;;
+  *)
+    echo "Invalid action: $action"
+    ;;
+esac
